9634bd3de1c7a8ebbbb1071174a08926f9fa46a4
Tomas Korcak <tomas.korcak@gooddata.com>
2014-04-12 17:03:54 +0200

    Temp Sensor added

--- snippets/lcdTemp.js
+++ snippets/lcdTemp.js
@@ -1,4 +1,6 @@
 var g = null;
+var ow = null;
+var sensor = null;
 
 var scaleValue = function(v, vMax) {
     return v * vMax;
@@ -9,25 +11,49 @@ var tick = function() {
 
     var light = analogRead(A1);
     var val = scaleValue(1 - light, 40);
-
-    msg = "Light: " + val;
-    console.log(JSON.stringify({
-        light: msg
-    }, null, 4));
+    var temp = sensor.getTemp();
 
     g.clear();
 
+    console.log(JSON.stringify({
+        light: val,
+        temp: temp
+    }, null, 4));
+
+
+    msg = "Light: " + val;
     g.drawString(msg, 0, 20);
+
+    msg = "Temp: " + temp;
+    g.drawString(msg, 0, 30);
+
     g.drawLine(0, val, 80, val);
 
     g.flip();
 };
 
+var initLcd = function() {
+  console.log('initLcd()');
+
+  SPI1.setup({ baud: 1000000, sck:B3, mosi:B5 });
+  g = require("PCD8544").connect(SPI1,B6,B7,B8);
+  g.setContrast(1);
+};
+
+var initTempSensor = function() {
+  console.log('initTempSensor()');
+
+  ow = new OneWire(B15);
+  sensor = require("DS18B20").connect(ow);
+};
+
 var init = function() {
     console.log("init()");
 
-    SPI1.setup({ baud: 1000000, sck:B3, mosi:B5 });
-    g = require("PCD8544").connect(SPI1,B6,B7,B8);
+    initLcd();
+
+    initTempSensor();
+
     setInterval(function() { tick(); }, 500);
 };
 
